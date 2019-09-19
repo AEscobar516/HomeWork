471af32571c57f79bba976b16438604b971dc994
AEscobar516 <55443534+AEscobar516@users.noreply.github.com>
2019-09-19 08:41:16 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/Main.cpp
@@ -0,0 +1,22 @@
+// Name: Alexander Escobar
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include <iostream>
+
+int main()
+{
+	std::cout   <<"Name:Alexander Escobar" << std::endl 
+	    	   	<<"Period 1: Comp Sci" << std::endl
+				<<"Period 2: Gym" << std::endl
+				<<"Period 3: House" << std::endl
+				<<"Period 4: Global I" << std::endl
+				<<"Period 5: Lunch" << std::endl
+				<<"Period 6: Health" << std::endl
+				<<"Period 7: English 10" << std::endl
+				<<"Period 8: Algebra II" << std::endl
+				<<"Period 9: Spanish II" << std::endl
+				<<"Period 10: Earth Science";
+}
